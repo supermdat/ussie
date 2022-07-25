61b695ed6d55e615abb6308dacceac2c66b63993
supermdat <turse.mda@gmail.com>
2022-07-25 11:48:20 -0400

    Updating package documentation.

--- R/matches.R
+++ R/matches.R
@@ -1,3 +1,17 @@
+#' Make a standard league-play tibble
+#'
+#' Given a league-play data frame from {engsoccer}, returns a tibble with
+#' standardised column-names and types, e.g. `date` is a `Date`.
+#'
+#' @param data_engsoc  obtained from {engsoccerdata}.
+#' @param country `character` scalar, specifies the league.
+#'
+#' @return a tibble with columns `country`, `date`, `season`, `tier`, `home`,
+#'    `visitor`, `goals_home`, `goals_visitor`.
+#' @export
+#'
+#' @examples
+#' uss_make_matches(data_engsoc = engsoccerdata::spain, country = "Spain")
 uss_make_matches <- function(data_engsoc, country = "Italy") {
   result <-
     data_engsoc |>
